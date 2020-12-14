e8ecb1bfbc9f927d263253901233dd7cb76e8e37
Musab <musabnazir@gmail.com>
2020-12-14 17:26:51 -0500

    added h2 db

---
+++ src/musab/db_access.clj
@@ -0,0 +1,21 @@
+(ns musab.db-access
+  (:require [next.jdbc :as jdbc]))
+
+(def db-spec {:dbtype "h2" :dbname "banking-db"})
+
+(def db-source (jdbc/get-datasource db-spec))
+
+(comment
+  (jdbc/execute!
+   db-source
+   ["create table Users (id int auto_increment primary key,
+   name varchar (255), email varchar (255))"])
+  
+  (jdbc/execute!
+   db-source
+   ["insert into Users (name, email) values 
+     ('Musab Nazir', 'musabnazir@gmail.com')"])
+  
+  (jdbc/execute!
+   db-source
+   ["select * from users"]))
